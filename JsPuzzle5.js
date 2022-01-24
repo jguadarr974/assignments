c47bfd654a2c2a91e1b88c25bf9bd87c7d70bfa9
jguadarr974 <78454204+jguadarr974@users.noreply.github.com>
2022-01-24 14:48:17 -0500

    JS puzzle #5- reverse words

---
+++ JsPuzzle5.js
@@ -0,0 +1,24 @@
+/* 
+Question: Write a function in Javascript that will reverse every 
+word in a given string and return the new string. Every word should be 
+reversed but the string as a whole should not be reversed. And DO NOT 
+use array.reverse() method
+*/
+
+/*
+1. given string, reverse every word 
+2. return the new string 
+3. string itself should not be reversed
+*/
+
+//Make a string 
+var name="jennifer";
+
+//have a variable with the same length as the string 
+//this way, it can print the last index to index 0, 
+//have the for loop decrease the len.
+// return the whole string
+for (var len = name.length-1; len >= 0; len--)
+{
+    return name;
+}
